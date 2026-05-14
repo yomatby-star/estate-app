08182d375fac5e72b9012a61c5b55c4196480705
的場 <yomatby@gmail.com>
2026-05-14 23:28:21 +0900

    部屋登録簡易

--- src/pages/registerPage/roomRegisterPage/RoomRegisterPage.tsx
+++ src/pages/registerPage/roomRegisterPage/RoomRegisterPage.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react"
+import { useSearchParams } from "react-router-dom"
+
 
 
 export default function RoomRegisterPage() {
+  const [searchParamsto] = useSearchParams()
+  console.log("searchParamsto", searchParamsto)
+
+  const buildingId = searchParamsto.get("buildingId")
+
+  const [selectedBuildingId, setSelectedBuildingId ] = useState(buildingId ?? "")
+
   return (
-    <div>RoomRegisterPage</div>
+    <div>
+      <div>
+        <h2>部屋登録</h2>
+        <div>選択中の物件ID: {selectedBuildingId || "未選択"}</div>
+      </div>
+    </div>
   )
 }
