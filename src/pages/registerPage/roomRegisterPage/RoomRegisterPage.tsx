0bcdefbde7111f163ff72df81c740a47c92363ba
user <user@usernoMacBook-Pro.local>
2026-05-22 01:12:59 +0900

    部屋登録途中まで

--- src/pages/registerPage/roomRegisterPage/RoomRegisterPage.tsx
+++ src/pages/registerPage/roomRegisterPage/RoomRegisterPage.tsx
@@ -1,46 +1,64 @@
 import { useState } from "react"
 import { useSearchParams } from "react-router-dom"
+import styles from "./RoomRegisterPage.module.css"
+import { Key } from "lucide-react"
 
 
 
 export default function RoomRegisterPage() {
   const [searchParamsto] = useSearchParams()
-  console.log("searchParamsto", searchParamsto)
-
   const buildingId = searchParamsto.get("buildingId")
-
   const [selectedBuildingId, setSelectedBuildingId ] = useState(buildingId ?? "")
 
+  const ENDPOINT = "http://127.0.0.1:8000"
+  const PATH = "/api/v1/roomRegister"
+  const ENDPOINT_URL = `${ENDPOINT}${PATH}`
+  // console.log("ENDPOINT_URL:", ENDPOINT_URL)
+
+  const INPUT_ITEMS = [
+    { label: "部屋番号", key: "roomNumber", required: true },
+    { label: "家賃", key: "roomRent", required: true },
+    { label: "ステータス", key: "roomStatus", required: true }
+  ] as const
+
+  const initialForm = {
+    roomNumber: "",
+    roomRent: "",
+    roomStatus: "",
+  }
+
+  const [form, setForm] = useState(initialForm)
+  const [isSaving, setIsSaving] = useState(false)
+
+  const onSave = () => console.log("保存をクリックしました")
+
+
   return (
-    <div>
-      <div>
-        <strong>部屋登録</strong>
-        <div>選択中の物件ID: {selectedBuildingId || "未選択"}</div>
+    <div className={styles.stack}>
+      <div className={styles.card}>
+        <div className={styles.title}>
+          <strong>部屋登録</strong>
+          <div>
+            物件名：{selectedBuildingId || "未選択"}
+          </div>
+        </div>
+        {INPUT_ITEMS.map(({ label, key, required }) => 
+          <div key={key} className={styles.inputFields}>
+            <span>{label}{required && "*"}</span>
+            <input
+              required={required}
+              value={form[key]}
+              onChange={(e) => 
+                setForm((prev) => ({
+                  ...prev,
+                  [key]: e.target.value
+                }))
+              }
+            />
+          </div>
+        )}
+        <button className={styles.save} onClick={onSave}>保存</button>
       </div>
-      <div>
-
-        <div>
-          <div>部屋番号</div>
-          <input 
-            value={"503"}
-          />
-        </div>
-
-        <div>
-          <div>家賃</div>
-          <input
-            type="number"
-            value={75000}
-          />
-        </div>
-
-        <div>
-          <div>ステータス</div>
-          <select>
-            
-          </select>
-        </div>
-      </div>
-    </div>
+    </div>  
   )
 }
