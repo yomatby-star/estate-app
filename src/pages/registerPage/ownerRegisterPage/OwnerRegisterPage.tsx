a9f51b3dbadb1d74c3ae7560eb4a9cba1f5b8e6f
user <user@usernoMacBook-Pro.local>
2026-05-24 14:39:01 +0900

    フロントオーナー登録途中まで

--- src/pages/registerPage/ownerRegisterPage/OwnerRegisterPage.tsx
+++ src/pages/registerPage/ownerRegisterPage/OwnerRegisterPage.tsx
@@ -3,7 +3,40 @@ import { useSearchParams } from "react-router-dom"
 
 
 export default function OwnerRegisterPage() {
+  const [searchParams] = useSearchParams()
+  const buildingId = searchParams.get("buildingId")
+
+  const [selectedBuildingId, setSelectedBuildingId] = useState(buildingId ?? "")
+  // console.log("buildingId", buildingId)
+
+  const INPUT_ITEMS = [
+    { label: "オーナー名", key: "ownerName", required: true },
+    { label: "住所", key: "ownerAddr", required: true }
+  ]
+
+  const initialForm = {
+    ownerName: "",
+    ownerAddr: "",
+  }
+
+  const [from, setForm] = useState(initialForm)
+
   return (
-    <div>テスト</div>
+    <div>
+      <div>
+        <div>
+          <strong>貸主登録</strong>
+          <div>物件名：{selectedBuildingId || "未選択"}</div>
+        </div>
+
+        <div>
+          {INPUT_ITEMS.map(({ label, key, required }) => 
+            <div key={key}>
+              <span>{label}{required && " *"}</span>              
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
