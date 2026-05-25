34015f6c5da7aacf44bce1d6f43ecc80fa8cd85e
user <user@usernoMacBook-Pro.local>
2026-05-25 20:27:55 +0900

    修正

--- src/pages/registerPage/ownerRegisterPage/OwnerRegisterPage.tsx
+++ src/pages/registerPage/ownerRegisterPage/OwnerRegisterPage.tsx
@@ -5,21 +5,18 @@ import { useSearchParams } from "react-router-dom"
 export default function OwnerRegisterPage() {
   const [searchParams] = useSearchParams()
   const buildingId = searchParams.get("buildingId")
-
   const [selectedBuildingId, setSelectedBuildingId] = useState(buildingId ?? "")
-  // console.log("buildingId", buildingId)
 
   const INPUT_ITEMS = [
     { label: "オーナー名", key: "ownerName", required: true },
-    { label: "住所", key: "ownerAddr", required: true }
-  ]
+    { label: "住所", key: "ownerAddr", required: true }  ] as const
 
   const initialForm = {
     ownerName: "",
     ownerAddr: "",
-  }
+  } as const
 
-  const [from, setForm] = useState(initialForm)
+  const [form, setForm] = useState(initialForm)
 
   return (
     <div>
@@ -28,13 +25,25 @@ export default function OwnerRegisterPage() {
           <strong>貸主登録</strong>
           <div>物件名：{selectedBuildingId || "未選択"}</div>
         </div>
-
         <div>
           {INPUT_ITEMS.map(({ label, key, required }) => 
             <div key={key}>
-              <span>{label}{required && " *"}</span>              
+              <span>{label}{required && " *"}</span>
+              <input
+                required={required}
+                value={form[key]}
+                onChange={(e) => 
+                  setForm((prev) => ({
+                    ...prev,
+                    [key]: e.target.value
+                  }))
+                }
+              />        
             </div>
           )}
+        </div>
+        <div>
+          <button>保存</button>
         </div>
       </div>
     </div>
