d0b181e6d7661a62edef8e51563c3f12f23a7f26
user <user@usernoMacBook-Pro.local>
2026-05-29 14:14:17 +0900

    画像イメージ修正

--- src/features/properties/components/propertyCard/mainCard/MainCard.tsx
+++ src/features/properties/components/propertyCard/mainCard/MainCard.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom"
 import type { Property } from "../../../../../mocks/properties/mock" 
 import { ROUTES } from "../../../../../routes/rouets"
 import styles from "./MainCard.module.css"
+import noImage from "../../../../../assets/no_image.jpeg"
 
 type Props = {
   property: Property;
@@ -11,21 +12,21 @@ export default function MainCard({ property }: Props) {
   // const vacantRoomsCount = property.roomStatus.filter((r) => r.status === "vacant").length
   const path = `${ROUTES.property}/${property.id}`
 
-  const LISTITEMS = [
-    { key: "buildingName", label: "物件名", value: property.basic.name },
-    { key: "buildingAddr", label: "住所", value: property.common.addr },
-    { key: "buildingLocal", label: "路線", value: property.common.local },
-    { key: "buildingStation", label: "最寄駅", value: property.common.station },
-    { key: "buildingMansionType", label: "種別", value: property.common.mansionType },
-    { key: "buildingStructure", label: "構造", value: property.common.structure },
+  const listItems = [
+    { id: "buildingName", label: "物件名", value: property.basic.name ?? "-" },
+    { id: "buildingAddr", label: "住所", value: property.common.addr ?? "-" },
+    { id: "buildingLocal", label: "路線", value: property.common.local ?? "-" },
+    { id: "buildingStation", label: "最寄駅", value: property.common.station ?? "-" },
+    { id: "buildingMansionType", label: "種別", value: property.common.mansionType ?? "-" },
+    { id: "buildingStructure", label: "構造", value: property.common.structure ?? "-" },
   ]
 
   return (
     <Link to={path} className={styles.card}>
-      <img src={property.imageUrl} className={styles.image} alt="物件写真" />
+      <img src={property.imageUrl || noImage} className={styles.image} alt="物件写真" />
       <div className={styles.itemsField}>
-        {LISTITEMS.map(({ key, label, value }) => 
-          <div key={key} className={styles.innerField}>
+        {listItems.map(({ id, label, value }) => 
+          <div key={id} className={styles.innerField}>
             <div className={styles.labelField}>
               <span>{label}</span>
             </div>
